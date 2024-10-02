bb50b9ca5caf1fe3f2e0631b22b99433f6a308a5
Juan_Angelo <angelo.flores@pucp.edu.pe>
2024-10-01 23:18:54 -0500

    Revert "Merge remote-tracking branch 'origin/Angelo-Back' into dev"
    This reverts commit d8b830d5e026d5c86db07c4cc5ff36084f69de27, reversing
    changes made to 88893917899e54edd86dcc06aa868814cf10e997.

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({isGlobal: true}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UserModule,
-    AuthModule,
-    
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
