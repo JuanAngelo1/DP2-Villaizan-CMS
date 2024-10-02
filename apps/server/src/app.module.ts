f91951e7cf5ff9514ad700ae3a765eef9c388c36
Juan_Angelo <angelo.flores@pucp.edu.pe>
2024-10-02 00:43:39 -0500

    feat(apps): add Prisma ORM, connection to DB

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,20 +1,10 @@
 import { Module } from '@nestjs/common';
-import { publicacionModule } from './publicacion/publicacion.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-  ],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
