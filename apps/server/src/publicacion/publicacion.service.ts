6792ad50f790a175549b2c93787497a4d7d4a544
gabo52 <gabrielduranruiz@gmail.com>
2024-10-04 17:55:22 -0500

    back(fix): Merging

--- apps/server/src/publicacion/publicacion.service.ts
+++ apps/server/src/publicacion/publicacion.service.ts
@@ -3,10 +3,14 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { cms_publicacion } from '@prisma/client';
 import { v4 as uuidv4 } from 'uuid';
 import { CreatePublicacionDto } from './dto/pub.dto';
+import { GoogleDriveHelper } from '../utils/google-drive.helper';
 
 @Injectable()
 export class PublicacionService {
-  constructor(private prisma: PrismaService) {}
+  private googleDriveHelper: GoogleDriveHelper;
+  constructor(private prisma: PrismaService) {
+    this.googleDriveHelper = new GoogleDriveHelper();
+  }
 
   async getAllPublicaciones(): Promise<cms_publicacion[]> {
     return this.prisma.cms_publicacion.findMany();
@@ -22,18 +26,30 @@ export class PublicacionService {
 
   async createPublicacion(
     data: CreatePublicacionDto,
+    file: Express.Multer.File,
   ): Promise<cms_publicacion> {
-    return this.prisma.cms_publicacion.create({
-      data: {
-        titulo: data.titulo,
-        urlimagen: data.urlimagen,
-        descripcion: data.descripcion,
-        fechapublicacion: data.fechapublicacion,
-        idcategoria: data.idcategoria,
-        idtipopublicacion: data.idtipopublicacion,
-        idestadopublicacion: data.idestadopublicacion,
-      },
-    });
+    try {
+      const imageUrl = await this.googleDriveHelper.uploadFile(
+        file.path,
+        file.originalname,
+      );
+
+      const nuevaPublicacion = await this.prisma.cms_publicacion.create({
+        data: {
+          titulo: data.titulo,
+          urlimagen: imageUrl,
+          descripcion: data.descripcion,
+          fechapublicacion: data.fechapublicacion,
+          idcategoria: data.idcategoria,
+          idtipopublicacion: data.idtipopublicacion,
+          idestadopublicacion: data.idestadopublicacion,
+        },
+      });
+      return nuevaPublicacion;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   }
 
   async updatePublicacion(
