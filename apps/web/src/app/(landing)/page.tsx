2a26e6dbfade026b15534312f93b0ed90f809788
RanulfoMurillo <rgmurillo@pucp.edu.pe>
2024-10-03 20:13:08 -0500

    front(feat) Publicaiones y noticias

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import Nosotros from "./_components/Nosotros";
+import Noticias from "./_components/Noticias";
 import Publicaciones from "./_components/Publicaciones";
 import Sabores from "./_components/Sabores";
 import Start from "./_components/Start";
@@ -14,6 +15,7 @@ export default function Page(): JSX.Element {
       <Sabores />
       <Nosotros />
       <Publicaciones />
+      <Noticias />
       <Footer />
     </main>
   );
