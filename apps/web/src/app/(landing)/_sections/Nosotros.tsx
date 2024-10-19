7fee480ad5dc2f736753d8cb4c0336c51c77cc08
rgpq25 <renzopq@hotmail.com>
2024-10-19 09:14:40 -0500

    landing(aboutus): Redesign, pending carousel and bg

--- apps/web/src/app/(landing)/_sections/Nosotros.tsx
+++ apps/web/src/app/(landing)/_sections/Nosotros.tsx
@@ -5,39 +5,54 @@ import MaxWidthWrapper from "../_components/MaxWidthWrapper";
 
 const Nosotros: React.FC = () => {
   return (
-    <section id="nosotros" className="bg-red-600 px-4 py-16 text-white font-['Abhaya_Libre']">
+    <section id="nosotros" className="bg-red-600 px-4 py-16 font-['Abhaya_Libre'] text-white">
       <MaxWidthWrapper>
-        <div className="grid gap-12 md:grid-cols-2">
-          {/* Misión */}
-          <div className="order-2 flex flex-col items-start md:order-1">
-            <h1 className="mb-4 md:text-7xl sm:text-5xl text-4xl">Misión</h1>
-            <p className="text-lg md:text-2xl">
-            Nuestra misión es ofrecer los mejores helados artesanales, elaborados con ingredientes naturales, seleccionados cuidadosamente para garantizar la frescura y calidad en cada paleta. Nos comprometemos a brindar una experiencia única que despierte los sentidos, rescatando los sabores tradicionales y creando nuevas combinaciones que sorprendan a nuestros clientes, todo en un entorno sostenible y responsable con el medio ambiente.
-            </p>
+        <div className="mt-[200px] flex h-screen flex-col items-center justify-start text-center">
+          <h1 className="mb-2 text-4xl sm:text-5xl md:text-7xl">Conoce un poco más sobre nosotros</h1>
+          <p className="text-lg md:text-2xl">
+            Paletas Villaizan se enorgullece de ser una marca dedicada a la creación de paletas artesanales
+            que cautivan y deleitan con sus sabores auténticos de frutas naturales y rellenos exquisitos que
+            endulzan el paladar. Con una trayectoria de 8 años en el Mercado de San Martín, en la exuberante
+            Selva del Perú, hemos arraigado nuestras raíces en la tradición y el saber hacer local.
+          </p>
+          <div className="mt-5 flex h-[50%] min-h-[300px] w-full flex-row gap-5 overflow-x-auto">
+            <img
+              src="/nosotros/aboutus1.jpg"
+              alt="Paletas Villaizan"
+              className="h-full w-fit rounded-md object-cover"
+            />
+            <img
+              src="/nosotros/aboutus6.jpg"
+              alt="Paletas Villaizan"
+              className="h-full w-fit rounded-md object-cover"
+            />
+            <img
+              src="/nosotros/aboutus7.jpg"
+              alt="Paletas Villaizan"
+              className="h-full w-fit rounded-md object-cover"
+            />
           </div>
-          {/* Imagen de misión */}
-          <div className="order-1 lg:w-[500px] md:w-[400px] w-[300px] md:order-2 place-self-end">
-            <AspectRatio ratio={6 / 4}>
-              <Image src="/nosotros/mision.png" alt="Misión" className="h-auto w-full rounded-lg" fill />
-            </AspectRatio>
-          </div>
+        </div>
 
-          {/* Imagen de visión */}
-          <div className="lg:w-[500px] md:w-[400px] w-[300px]">
-            <AspectRatio ratio={6 / 4}>
-              <Image
-                src="/nosotros/vision.png"
-                alt="Visión"
-                fill
-                className="h-auto w-full rounded-lg"
-              />
-            </AspectRatio>
-          </div>
-          {/* Visión */}
-          <div className="flex flex-col items-start text-right md:items-end">
-            <h1 className="mb-4 md:text-7xl sm:text-5xl text-4xl">Visión</h1>
-            <p className="text-lg md:text-2xl">Ser reconocidos como la heladería líder en innovación y calidad artesanal, ofreciendo productos que no solo deleitan el paladar, sino que también promueven un estilo de vida consciente y saludable. Nos esforzamos por expandir nuestro alcance, llevando nuestras paletas a nuevas comunidades, siempre manteniendo nuestro compromiso con la autenticidad y el respeto por las tradiciones locales.</p>
-          </div>
+        <div className="flex h-dvh w-[700px] flex-col items-start justify-center text-left">
+          <h1 className="mb-2 text-4xl sm:text-5xl md:text-7xl">Cual es nuestra visión?</h1>
+          <p className="text-lg md:text-2xl">
+            Ser reconocidos como la heladería líder en innovación y calidad artesanal, ofreciendo productos
+            que no solo deleitan el paladar, sino que también promueven un estilo de vida consciente y
+            saludable. Nos esforzamos por expandir nuestro alcance, llevando nuestras paletas a nuevas
+            comunidades, siempre manteniendo nuestro compromiso con la autenticidad y el respeto por las
+            tradiciones locales.
+          </p>
+        </div>
+        <div className="flex h-dvh w-[700px] flex-col items-start justify-center text-left">
+          <h1 className="mb-4 text-4xl sm:text-5xl md:text-7xl">Misión</h1>
+          <p className="text-lg md:text-2xl">
+            Nuestra misión es ofrecer los mejores helados artesanales, elaborados con ingredientes naturales,
+            seleccionados cuidadosamente para garantizar la frescura y calidad en cada paleta. Nos
+            comprometemos a brindar una experiencia única que despierte los sentidos, rescatando los sabores
+            tradicionales y creando nuevas combinaciones que sorprendan a nuestros clientes, todo en un
+            entorno sostenible y responsable con el medio ambiente.
+          </p>
         </div>
       </MaxWidthWrapper>
     </section>
