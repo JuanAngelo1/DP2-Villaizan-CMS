36d69dc91c1be78d71ae800229859d559109c029
RanulfoMurillo <rgmurillo@pucp.edu.pe>
2024-10-24 13:19:37 -0500

    feat: add image insert

--- apps/web/src/app/_components/TextEditor.tsx
+++ apps/web/src/app/_components/TextEditor.tsx
@@ -1,10 +1,13 @@
 import React, { useCallback, useState } from "react";
-import { useEditor, EditorContent, Editor, BubbleMenu } from "@tiptap/react";
+import { useEditor, EditorContent, Editor, BubbleMenu, FloatingMenu } from "@tiptap/react";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
+import Image from "@tiptap/extension-image";
 import Text from "@tiptap/extension-text";
 import Link from "@tiptap/extension-link";
 import Bold from "@tiptap/extension-bold";
+import TextAlign from '@tiptap/extension-text-align';
+import Heading from "@tiptap/extension-heading"
 import Underline from "@tiptap/extension-underline";
 import Italic from "@tiptap/extension-italic";
 import Strike from "@tiptap/extension-strike";
@@ -12,7 +15,22 @@ import Code from "@tiptap/extension-code";
 import History from "@tiptap/extension-history";
 import content from "../admin/contenido/_components/publicaciones_components/content";
 import { cn } from "@repo/ui/lib/utils"; // Asegúrate de que esta ruta sea correcta
-import { BoldIcon, Code2, ItalicIcon, Redo2, Strikethrough, UnderlineIcon, Undo2 } from "lucide-react";
+import { AlignCenterIcon, AlignJustifyIcon, AlignLeftIcon, AlignRightIcon, BoldIcon, Code2, ImageIcon, ItalicIcon, MinusIcon, PlusIcon, Redo2, Strikethrough, Trash2Icon, UnderlineIcon, Undo2, VideoIcon } from "lucide-react";
+import API from './api';  // aPI PARA SUBIR IMGS
+
+const CustomImage = Image.extend({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      width: {
+        default: '300px',
+        renderHTML: (attributes) => ({
+          style: `width: ${attributes.width}; display: block; margin-left: auto; margin-right: auto;`, // Centrar la imagen
+        }),
+      },
+    };
+  },
+});
 
 export default function TextEditor() {
   const editor = useEditor({
@@ -21,12 +39,18 @@ export default function TextEditor() {
       History,
       Paragraph,
       Text,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+        alignments: ['left', 'center', 'right', 'justify'],
+      }),
       Link.configure({
         openOnClick: false
       }),
       Bold,
       Underline,
       Italic,
+      CustomImage,
+      Image,
       Strike,
       Code
     ],
@@ -86,21 +110,74 @@ export default function TextEditor() {
     editor.chain().focus().toggleCode().run();
   }, [editor]);
 
+    // Alineación del texto
+  const alignLeft = useCallback(() => {
+    editor.chain().focus().setTextAlign('left').run();
+  }, [editor]);
+
+  const alignCenter = useCallback(() => {
+    editor.chain().focus().setTextAlign('center').run();
+  }, [editor]);
+
+  const alignRight = useCallback(() => {
+    editor.chain().focus().setTextAlign('right').run();
+  }, [editor]);
+
+  const alignJustify = useCallback(() => {
+    editor.chain().focus().setTextAlign('justify').run();
+  }, [editor]);
+
+  const insertImage = async () => {
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = 'image/*';
+    
+    fileInput.onchange = async (event) => {
+      const file = event.target.files?.[0];
+      if (file && editor) {
+        const imageUrl = await API.uploadImage(file);
+        editor.chain().focus().setImage({ src: imageUrl }).run();
+      }
+    };
+
+    fileInput.click();
+  };
+
+  const increaseImageSize = () => {
+      const currentAttrs = editor.getAttributes('image');
+      const currentWidth = currentAttrs.width || '300px';
+      const newWidth = `${parseInt(currentWidth) + 50}px`;
+
+      editor.chain().focus().setImage({ ...currentAttrs, width: newWidth }).run();
+    };
+
+    const decreaseImageSize = () => {
+      const currentAttrs = editor.getAttributes('image');
+      const currentWidth = currentAttrs.width || '300px';
+      const newWidth = `${Math.max(50, parseInt(currentWidth) - 50)}px`;
+
+      editor.chain().focus().setImage({ ...currentAttrs, width: newWidth }).run();
+    };
+
+    const removeImage = () => {
+      editor.chain().focus().deleteSelection().run();
+    };
+
   if (!editor) {
     return null;
   }
 
   return (
     <div className="relative w-full mb-12">
-      {/* BubbleMenu para Formateo de Texto */}
+      {/* BubbleMenu para Formateo de Texto*/}
       <BubbleMenu
         pluginKey="bubbleMenuText"
         className="flex items-center gap-2 p-2 bg-accent text-whiteCustom rounded shadow-lg"
         tippyOptions={{ duration: 150 }}
         editor={editor}
-        shouldShow={({ editor, view, state, oldState, from, to }) => {
-          // Solo mostrar si hay un rango seleccionado.
-          return from !== to;
+        shouldShow={({ editor }) => {
+          // Mostrar solo si NO es una imagen activa.
+          return !editor.isActive('image') && editor.state.selection.from !== editor.state.selection.to;
         }}
       >
         <button
@@ -172,6 +249,30 @@ export default function TextEditor() {
         >
           <Code2 />
         </button>
+        <button
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-transparent cursor-pointer hover:bg-gray5"
+          onClick={alignLeft}
+        >
+          <AlignLeftIcon />
+        </button>
+        <button
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-transparent cursor-pointer hover:bg-gray5"
+          onClick={alignCenter}
+        >
+          <AlignCenterIcon />
+        </button>
+        <button
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-transparent cursor-pointer hover:bg-gray5"
+          onClick={alignRight}
+        >
+          <AlignRightIcon />
+        </button>
+        <button
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-transparent cursor-pointer hover:bg-gray5"
+          onClick={alignJustify}
+        >
+          <AlignJustifyIcon />
+        </button>
       </BubbleMenu>
 
       {/* BubbleMenu para Edición de Enlaces */}
@@ -180,9 +281,9 @@ export default function TextEditor() {
         className="flex items-center gap-2 p-2 bg-gray1 text-whiteCustom rounded shadow-lg"
         tippyOptions={{ duration: 150 }}
         editor={editor}
-        shouldShow={({ editor, view, state, oldState, from, to }) => {
-          // Solo mostrar el bubble menu para enlaces.
-          return from === to && editor.isActive("link");
+        shouldShow={({ editor }) => {
+           // Solo mostrar el bubble menu para enlaces.
+          return !editor.isActive('image') && editor.isActive("link");
         }}
       >
         <button
@@ -198,6 +299,55 @@ export default function TextEditor() {
           Remove
         </button>
       </BubbleMenu>
+
+      {/* BubbleMenu para edición de imágenes */}
+      <BubbleMenu
+        pluginKey="bubbleMenuImage"
+        className="flex items-center gap-2 p-2 bg-white shadow-md rounded"
+        editor={editor}
+        tippyOptions={{ duration: 150 }}
+        shouldShow={({ editor }) => editor.isActive('image')}  // Solo mostrar si una imagen está activa
+      >
+        <button
+          onClick={increaseImageSize}
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-gray-200 hover:bg-gray-300"
+        >
+          <PlusIcon />
+        </button>
+        <button
+          onClick={decreaseImageSize}
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-gray-200 hover:bg-gray-300"
+        >
+          <MinusIcon />
+        </button>
+        <button
+          onClick={removeImage}
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-red-500 text-white hover:bg-red-600"
+        >
+          <Trash2Icon />
+        </button>
+      </BubbleMenu>
+
+       {/* FloatingMenu para insertar elementos como imágenes */}
+      <FloatingMenu
+        editor={editor}
+        tippyOptions={{ duration: 150 }}
+        className="flex gap-2 p-2 bg-white shadow-md rounded absolute left-0"
+        shouldShow={({ editor, state }) => {
+          const { $from } = state.selection;
+          return $from.parent.type.name === 'paragraph';
+        }}
+      >
+        <button
+          className="flex items-center justify-center w-8 h-8 p-0 rounded bg-gray5 hover:bg-gray4"
+          onClick={insertImage}
+        >
+          <ImageIcon />
+        </button>
+        <button className="flex items-center justify-center w-8 h-8 p-0 rounded bg-gray5 hover:bg-gray4">
+          <PlusIcon />
+        </button>
+      </FloatingMenu>
 
       {/* Contenido del Editor */}
       <EditorContent
