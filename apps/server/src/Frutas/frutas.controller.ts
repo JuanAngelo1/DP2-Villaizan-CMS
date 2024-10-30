ccbff48189927dd35aa856b6bd7671f5fc5d958f
Juan_Angelo <angelo.flores@pucp.edu.pe>
2024-10-29 21:50:56 -0500

    back(fix): Hotfix app module

--- apps/server/src/Frutas/frutas.controller.ts
+++ apps/server/src/Frutas/frutas.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Body,
+    Res,
+    Param,
+    Req,
+    NotFoundException,
+    ParseIntPipe,
+} from '@nestjs/common';
+import { FrutasService } from './frutas.service';
+import { Response } from 'express';
 
 @Controller('frutas')
-export class FrutasController {}
+export class FrutasController {
+
+    constructor(private readonly frutasService: FrutasService) {}
+
+    @Get()
+    async getFAQS(
+      @Req() request: Request,
+      @Res() response: Response,
+    ): Promise<any> {
+      try {
+        const result = await this.frutasService.getAllFrutas();
+        return response.status(200).json({
+          status: 'Success',
+          message: 'Frutas encontrados',
+          result: result,
+        });
+      } catch (err) {
+        return response.status(500).json({
+          status: 'Error',
+          message: 'Error al obtener las Frutas',
+          result: [],
+        });
+      }
+    }
+
+}
--- apps/server/src/Frutas/frutas.service.ts
+++ apps/server/src/Frutas/frutas.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { vi_fruta } from '@prisma/client';
+import { vi_producto } from '@prisma/client';
+import { vi_promocion } from '@prisma/client';
+import { vi_producto_fruta } from '@prisma/client';
+
 
 @Injectable()
-export class FrutasService {}
+export class FrutasService {
+
+    constructor(private prisma: PrismaService) {}
+
+    async getAllFrutas() {
+        return this.prisma.vi_fruta.findMany({
+          where: {
+            estaactivo: true,
+          },
+          select: {
+            nombre: true,
+            urlimagen: true,
+            descripcion: true,
+          },
+        });
+      }
+
+}
